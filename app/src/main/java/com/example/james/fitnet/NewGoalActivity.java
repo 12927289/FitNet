b9545a07554f98e6e8bf95d9c60ad673dfe93b28
Baqer Mamouri <baqer@Baqers-MacBook-Pro.local>
2018-04-27 00:38:02 +1000

    Adding a rudimentary database declaration using Room and sqlite

--- app/src/main/java/com/example/james/fitnet/NewGoalActivity.java
+++ app/src/main/java/com/example/james/fitnet/NewGoalActivity.java
@@ -6,6 +6,44 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.arch.persistence.room.RoomDatabase;
+
+// https://developer.android.com/training/data-storage/room/
+@Entity
+public class Goal {
+    @PrimaryKey
+    private int uid;
+
+    @ColumnInfo(name = "title")
+    private String title;
+}
+
+@Dao
+public interface GoalDao {
+    @Query("SELECT * FROM goal")
+    List<User> getAll();
+
+    @Query("SELECT * FROM goal WHERE uid IN (:goalIds)")
+    List<User> loadAllByIds(int[] userIds);
+
+    @Query("SELECT * FROM goal WHERE title LIKE :title LIMIT 1")
+    User findByName(String title);
+
+    @Insert
+    void insertAll(Goal... goals);
+
+    @Delete
+    void delete(Goal goal);
+}
+
+@Database(entities = {Goal.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+    public abstract GoalDao userDao();
+}
+
+AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+        AppDatabase.class, "fitnet").build();
+
 
 public class NewGoalActivity extends AppCompatActivity {
 
